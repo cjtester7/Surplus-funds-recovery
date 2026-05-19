f276fb9ea1a0e0b0930cbdb8de58fa00e92afcdc
cjtester7 <cjtester7@gmail.com>
2026-05-18 21:35:12 -0400

    Delete src directory

--- src/firebase.ts
+++
@@ -1,9 +0,0 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import firebaseConfig from '../firebase-applet-config.json';
-
-const app = initializeApp(firebaseConfig);
-// The firestoreDatabaseId is required for Enterprise Edition databases created by the tool
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
-export const auth = getAuth(app);
